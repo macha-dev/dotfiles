1b26f343d927106479d7cf8a984aa9dda92dc0fd
wimble <chastonmax@gmail.com>
2022-01-20 23:00:17 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp -i .Xresources ~
+cp -i i3/config ~/.config/i3/config
+cp -i redarch.png ~/Pictures
+cp -i Firefox_wallpaper.png ~/Pictures
+cp -i i3status/config ~/.config/i3status/config
+cp -r -i nitrogen ~/.config/
